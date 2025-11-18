738fa7a91e1d6968a8b63ffecd0c9d98e8e13615
unlikelyUsual <kumarprabhat308@gmail.com>
2025-11-18 18:21:34 +0530

    Failed test case fix

--- app/utils/files.ts
+++ app/utils/files.ts
@@ -1,11 +1,16 @@
-import { existsSync } from "node:fs";
+import { accessSync, constants } from "node:fs";
 import { join } from "node:path";
 import { PATHS } from "../main";
 
 export const getExecutableDirectories = (fileName: string): string => {
   for (const directory of PATHS) {
     const fullPath = join(directory, fileName);
-    if (existsSync(fullPath)) return fullPath;
+    try {
+      accessSync(fullPath, constants.X_OK);
+      return fullPath;
+    } catch (error) {
+      continue;
+    }
   }
   return "";
 };
